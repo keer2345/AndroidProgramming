f181b4d36bf997f9e683b67f1c5e0458545b2cde
keer2345 <keer2345@gmail.com>
2017-05-29 23:53:29 +0800

    add Question.java and NextButton event

---
+++ GeoQuiz/app/src/main/java/com/keer/android/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package com.keer.android.geoquiz;
+
+/**
+ * Created by qinjh on 17-5-29.
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int mTextResId, boolean mAnswerTrue) {
+        this.mTextResId = mTextResId;
+        this.mAnswerTrue = mAnswerTrue;
+    }
+
+    public int getmTextResId() {
+        return mTextResId;
+    }
+
+    public void setmTextResId(int mTextResId) {
+        this.mTextResId = mTextResId;
+    }
+
+    public boolean ismAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setmAnswerTrue(boolean mAnswerTrue) {
+        this.mAnswerTrue = mAnswerTrue;
+    }
+}
