342c01e73ffa0f9152258b26f81dfe38ed70927c
keer2345 <keer2345@gmail.com>
2017-05-30 09:27:18 +0800

    add Preview Button and this event

--- GeoQuiz/app/src/main/java/com/keer/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/keer/android/geoquiz/QuizActivity.java
@@ -12,6 +12,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mPrevButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[]{
@@ -26,6 +27,13 @@ public class QuizActivity extends AppCompatActivity {
 
     private void clickNextQuestion() {
         mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+    }
+
+    private void clickPrevQuestion() {
+        if (mCurrentIndex == 0) {
+            mCurrentIndex = 5;
+        }
+        mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
     }
 
     private void updateQuestion() {
@@ -85,6 +93,15 @@ public class QuizActivity extends AppCompatActivity {
             public void onClick(View v) {
 //                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
                 clickNextQuestion();
+                updateQuestion();
+            }
+        });
+
+        mPrevButton = (Button) findViewById(R.id.preview_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickPrevQuestion();
                 updateQuestion();
             }
         });
