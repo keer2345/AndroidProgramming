7b3456fa1c1e5d3a7297d99d7a8a3afcc956c598
keer2345 <keer2345@gmail.com>
2017-06-21 08:35:38 +0800

    add SingleFragmentActivity.java

---
+++ CriminalIntent/app/src/main/java/com/keer/android/criminalintent/SingleFragmentActivity.java
@@ -0,0 +1,31 @@
+package com.keer.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by qinjh on 17-6-21.
+ */
+
+public abstract class SingleFragmentActivity extends FragmentActivity {
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
