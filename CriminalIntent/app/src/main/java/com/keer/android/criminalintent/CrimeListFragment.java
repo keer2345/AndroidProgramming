119fbed99d0a3230b0f5931e0b5183ef098f6cc0
keer2345 <keer2345@gmail.com>
2017-06-21 09:30:10 +0800

    add CrimeListActivity.java and CrimeListFragment.java

---
+++ CriminalIntent/app/src/main/java/com/keer/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package com.keer.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by qinjh on 17-6-21.
+ */
+
+public class CrimeListFragment extends Fragment {
+}
